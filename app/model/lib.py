614a160941ff7d759cd81c8dbf982a9f008119ae
genkunabe@hotmail.com <genkunabe@hotmail.com>
2016-07-31 16:10:16 +0800

    finish library bugs fixing

--- app/model/lib.py
+++ app/model/lib.py
@@ -47,8 +47,8 @@ class Library:
 
 		return books
 
-	def get_book_detail(self, url, ff):
-		uri = urls['main'] + url + '&FF=' + ff
+	def get_book_detail(self, url, ff=None):
+		uri = urls['main'] + url + (('&FF=' + ff) if not ff == None else '')
 		result = self.opener.open(uri)
 		html = result.read()
 
@@ -56,35 +56,65 @@ class Library:
 		lines = re.findall(pattern, html)
 
 		detail = []
-		for line in lines:
-			pattern = re.compile(r'briefcitTitle">.+?href.+?">(.+?)</a>', re.S)
-			title = re.findall(pattern, line)[0]
+		if not len(lines) == 0:
+			for line in lines:
+				pattern = re.compile(r'briefcitTitle">.+?href.+?">(.+?)</a>', re.S)
+				title = re.findall(pattern, line)[0]
 
-			pattern = re.compile(r'<br />\s*(.+?)<br />\s*(.+?)<br />', re.S)
-			author, press = re.findall(pattern, line)[0]
+				pattern = re.compile(r'<br />\s*(.+?)<br />\s*(.+?)<br />', re.S)
+				author, press = re.findall(pattern, line)[0]
 
-			pattern = re.compile(r'<tr  class="bibItemsEntry">\s*(.+?)</tr>', re.S)
-			indexs = re.findall(pattern, line)
+				status = self.status_parser(line)
+				detail.append([title, author, press, status])
+		else:
+			pattern = re.compile(r'<!-- next row for fieldtag=a -->(.+?)<!-- next row for fieldtag=t -->(.+?)<!-- next row for fieldtag=p -->(.+?)<!-- END INNER BIB TABLE -->', re.S)
+			author, title, press = re.findall(pattern, html)[0]
 
-			status = []
-			for index in indexs:
-				pattern = re.compile(r'&nbsp;(.+?)\s*</td>', re.S)
-				items = re.findall(pattern, index)
+			pattern = re.compile(r'href.+?">(.+?)</a>', re.S)
+			author = re.findall(pattern, author)[0]
+			press = re.findall(pattern, press)[0]
 
-				pattern = re.compile(r'>(.+?)</a>', re.S)
-				items[1] = re.findall(pattern, items[1])[0]
-				status.append(items)
+			pattern = re.compile(r'<strong>(.+?)</strong>', re.S)
+			title = re.findall(pattern, title)[0]
+
+			status = self.status_parser(html)
+			status = status[0:len(status) / 2]
 			detail.append([title, author, press, status])
 
 		return detail
 
+	def status_parser(self, html):
+		pattern = re.compile(r'<tr  class="bibItemsEntry">\s*(.+?)</tr>', re.S)
+		indexs = re.findall(pattern, html)
+
+		status = []
+		for index in indexs:
+			pattern = re.compile(r'&nbsp;(.+?)\s*</td>', re.S)
+			items = re.findall(pattern, index)
+
+			pattern = re.compile(r'>(.+?)</a>', re.S)
+			items[1] = re.findall(pattern, items[1])[0]
+			status.append(items)
+
+		return status
+
 
 if __name__ == '__main__':
 	library = Library()
-	books = library.get_book_list('机器学习')
+	books = library.get_book_list('机器学习'.decode('utf8'))
+
+	detail = library.get_book_detail(books[1][0])
+	for d in detail:
+		print d[0]
+		print d[1], d[2]
+		for dd in d[3]:
+			print dd[0], dd[1], dd[2]
+		print '\n\n'
+
+	exit()
 
 	for book in books:
-		# print book[0], book[1]
+		print book[0], book[1]
 		detail = library.get_book_detail(book[0])
 		for d in detail:
 			print d[0]
