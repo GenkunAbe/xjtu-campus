d8d79dfb692973a65a4bb4220004af356b10c9e8
GenkunAbe <genkunabe@hotmail.com>
2016-08-01 10:51:28 +0800

    return to refresh grade

--- app/ctrl/grade.py
+++ app/ctrl/grade.py
@@ -11,11 +11,11 @@ class GradeCtrl(tornado.web.RequestHandler):
   def get(self):
     usr = self.get_argument('usr')
     psw = self.get_argument('psw')
-    #ssfw = Ssfw(usr, psw)
-    #grades = ssfw.get_grades()
-    # self.write(json.dumps(grades))
-    f = open('tmpgrade.json', 'r')
-    self.write(f.read())
+    ssfw = Ssfw(usr, psw)
+    grades = ssfw.get_grades()
+    self.write(json.dumps(grades))
+    # f = open('tmpgrade.json', 'r')
+    # self.write(f.read())
 
   def post(self):
     self.write('Not Finish!')
