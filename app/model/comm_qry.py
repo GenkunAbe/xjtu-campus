749259a9d5e6178b2611ac7bc23a5983db55987f
GenkunAbe <genkunabe@hotmail.com>
2017-02-19 12:34:22 +0800

    new term

--- app/model/comm_qry.py
+++ app/model/comm_qry.py
@@ -4,7 +4,7 @@ import sys
 from datetime import date
 
 def get_now_week():
-    delta = date.today() - date(2016, 9, 5)
+    delta = date.today() - date(2017, 2, 20)
     return delta.days / 7 + 1
 
 if __name__ == '__main__':
